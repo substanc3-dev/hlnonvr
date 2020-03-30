4905856ff8f8573685dbc40197e4c2d9e2d753b6
Jan4V <jano4varga@gmail.com>
2020-03-30 19:43:04 +0200

    Initial push

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # hlnonvr
-Run FPS-style Non-VR mods on top of Source2
+Run FPS-style Non-VR mods on top of Source2 (targets Half-Life: Alyx)
+
+-----
+
+ - Adds non-vr support to modern versions of the game
+ - Adds support for loading mods using `-game`
+ - Enables all convars (adds back `vr_enable_fake_vr_test` but also pretty nice for datamining schema stuff using `schema_` commands)
+ - Support for all versions of the game (tested on 1.1, 1.2 and the preload non-vr supporting version)
+ - Support for dedicated servers (using `-dedicated -allow_no_lobby_connect`)
+ - Support for running mods in VR mode using `-vr`
+
+# Usage:
+ - Place this dll into `Half-Life Alyx\game\bin\win64`
+ - Create a copy of "hlvr.exe" called whatever you would like (gonna be using "hlnonvr.exe" in this example) 
+ - Open the executable ("hlnonvr.exe") in a hex editor
+ - Find the string `engine2` (not the string `Failed to load the engine2 DLL` though) (in 1.2 it's at 0x10FB8)
+ - Replace (don't add any bytes, overwrite existing ones) the string `engine2` with `hlnonvr`
+ - You can now use this executable to run HLA (or mods using `-game`) in nonvr mode (no `-nonvr` flag or similar needed)
+
---
+++ src/dllmain.cpp
@@ -0,0 +1,92 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+#include <Minhook.h>
+#include <thread>
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    return TRUE;
+}
+
+#define EXTERN_DLL_EXPORT extern "C" __declspec(dllexport)
+const char* message = "Created by Jan4V, do not redistribute without prior permission.";
+
+typedef void(__fastcall* source2)(HINSTANCE, HINSTANCE, LPWSTR, long long, const char*, const char*);
+typedef char(__fastcall* vrinit)(char*);
+typedef __int64(__fastcall* dedicatedinit)(__int64, __int64, __int64);
+typedef __int64(__fastcall* cl_cvar)(__int64, __int64);
+
+LPVOID origVRInit, origDedicatedServer, origConvarSetup;
+
+
+char __fastcall VRInitHook(char* settings)
+{
+    settings[299] = 0; //VRMode = false;
+
+    return ((vrinit)origVRInit)(settings);
+}
+__int64 __fastcall DedicatedServerHook(__int64 rcx, __int64 rdx, __int64 r8)
+{
+    *(DWORD*)(rdx + 100) = 64; //MaxMaxPlayers = 64;
+    *(char*)(rdx + 88) = 1; //SteamMode = true;
+    
+    return ((dedicatedinit)origDedicatedServer)(rcx, rdx, r8);
+}
+
+__int64 __fastcall ConvarSetupHook(__int64 a1, __int64 a2)
+{
+    *(uint64_t*)(a2 + 40) &= ~3; //Flags &= ~3; // removes hidden and dev flags
+
+    return ((cl_cvar)origConvarSetup)(a1, a2);
+}
+
+EXTERN_DLL_EXPORT void __fastcall Source2Main(HINSTANCE a, HINSTANCE b, LPWSTR c, long long d, const char* e, const char* f)
+{
+    auto lib2 = LoadLibraryExA("vstdlib.dll", NULL, 8);
+    MH_Initialize();
+    //0x9AF0
+    MH_CreateHook((LPVOID)(((unsigned long long)lib2) + 0x9AB1 + strlen(message)), &ConvarSetupHook, &origConvarSetup);
+    MH_EnableHook((LPVOID)(((unsigned long long)lib2) + 0x9AB1 + strlen(message)));
+
+    auto lib = LoadLibraryExA("engine2.dll", NULL, 8);
+    source2 init = (source2)GetProcAddress(lib, "Source2Main");
+
+    char* line = GetCommandLineA();
+
+    if (!strstr(line, "-dedicated"))
+    {
+        if (!strstr(line, "-vr"))
+        {
+            //0x103760
+            MH_CreateHook((LPVOID)(((unsigned long long)lib) + 0x103721 + strlen(message)), &VRInitHook, &origVRInit);
+            MH_EnableHook((LPVOID)(((unsigned long long)lib) + 0x103721 + strlen(message)));
+        }
+    }
+    else
+    {
+        //0x636D0
+        MH_CreateHook((LPVOID)(((unsigned long long)lib) + 0x63691 + strlen(message)), &DedicatedServerHook, &origDedicatedServer);
+        MH_EnableHook((LPVOID)(((unsigned long long)lib) + 0x63691 + strlen(message)));
+    }
+    
+    auto modParam = strstr(line, "-game ");
+    if (modParam != 0)
+    {
+        modParam += 6;
+
+        auto len = strchr(modParam, ' ');
+
+        char* modname = new char[(int)(len - modParam) + 1];
+
+        memcpy(modname, modParam, (int)(len - modParam));
+        modname[(int)(len - modParam)] = '\0';
+
+        init(a, b, c, d, e, modname);
+    }
+    else
+        init(a, b, c, d, e, f);
+
+}
